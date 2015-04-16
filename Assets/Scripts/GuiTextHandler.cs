0171323067cde6d0706760aa349e533564e67afd
roger-acm <rhu1@students.kennesaw.edu>
2015-04-16 19:33:55 -0400

    new donut, ice cream, background sounds, started GUI text

---
+++ Assets/Scripts/GuiTextHandler.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GuiTextHandler : MonoBehaviour {
+
+    //public GameObject gui;
+    static int score = 0;
+    public int test;
+
+	void OnGUI () {
+
+        GUI.Box(new Rect(Screen.height/2, Screen.width/2, 100, 30), "" + score);
+	}
+
+    void Update()
+    {
+        test = score;
+    }
+}
