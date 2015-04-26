a9d946c0f373a4ac89c5a70d7fdc5a3b120b3a3d
roger-acm <rhu1@students.kennesaw.edu>
2015-04-26 13:14:46 -0400

    custard.blend, send score script

---
+++ Assets/Scripts/SendScoreScript.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using System.Collections;
+
+namespace Assets.Scripts
+{
+    public class SendScoreScript : MonoBehaviour
+    {
+
+        void Start()
+        {
+            int score = PlayerCounter.PlayerScore;
+            string url = "http://www.example.com/unityupdatescript.php?id=ROG&amp;score="+score;
+            WWW www = new WWW(url);
+            StartCoroutine(WaitForRequest(www));
+        }
+
+        IEnumerator WaitForRequest(WWW www)
+        {
+            yield return www;
+
+            // check for errors
+            if (www.error == null)
+            {
+                Debug.Log("WWW Ok!: " + www.text);
+            }
+            else
+            {
+                Debug.Log("WWW Error: " + www.error);
+            }
+        }
+    }
+}
